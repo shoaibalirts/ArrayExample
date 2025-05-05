fbf0c38673106d04966332175369974d89c87a8c
shoaibalirts <shoaibali.rts@gmail.com>
2025-05-05 18:23:54 +0200

    a is array reference cariable, 30 is the value to be searched out, and 3 is the index from where value to be searched out

--- ArrayExample/Program.cs
+++ ArrayExample/Program.cs
@@ -6,7 +6,7 @@ namespace ArrayExample
     {
         static void Main(string[] args)
         {
-            int[] a = new int[5] { 10, 20, 30, 40, 50 };
+            int[] a = new int[6] { 10, 20, 30, 40, 50, 30 };
             string[] b = new string[2] { "Shoaib", "Ali" };
             foreach (int i in a)
             {
@@ -16,6 +16,9 @@ namespace ArrayExample
             {
                 Console.WriteLine(i);
             }
+            // a is array reference cariable, 30 is the value to be searched out, and 3 is the index from where value to be searched out
+            int getIndex = Array.IndexOf(a, 30, 3);// search start from index 3. index 3 is included
+            Console.WriteLine("the value of " + 30 + " is found at " + getIndex);
         }
     }
 }
