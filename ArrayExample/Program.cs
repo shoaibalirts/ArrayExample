9b5795de10ed6876f48c6f10ac36c0260c2a5150
shoaibalirts <shoaibali.rts@gmail.com>
2025-05-05 18:34:51 +0200

    same as IndexOf. Binary search must be pre-sorted before search

--- ArrayExample/Program.cs
+++ ArrayExample/Program.cs
@@ -19,6 +19,13 @@ namespace ArrayExample
             // a is array reference cariable, 30 is the value to be searched out, and 3 is the index from where value to be searched out
             int getIndex = Array.IndexOf(a, 30, 3);// search start from index 3. index 3 is included
             Console.WriteLine("the value of " + 30 + " is found at " + getIndex);
+            int[] arr = new int[10] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 }; // pre-sorted
+            getIndex = Array.BinarySearch(arr, 30); // same as IndexOf. Binary search must be pre-sorted before search
+            Console.WriteLine("the value of " + 30 + " is found at " + getIndex);
+            getIndex = Array.BinarySearch(arr, 80);
+            Console.WriteLine("the value of " + 80 + " is found at " + getIndex);
+
+
         }
     }
 }
