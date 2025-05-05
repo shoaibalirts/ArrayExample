7050a128aefeda956df453ea586397404d5536fd
shoaibalirts <shoaibali.rts@gmail.com>
2025-05-05 18:04:45 +0200

    for is based on index. foreach is based on sequence

--- ArrayExample/Program.cs
+++ ArrayExample/Program.cs
@@ -6,8 +6,16 @@ namespace ArrayExample
     {
         static void Main(string[] args)
         {
-            int[] a = new int[5];
-            Console.WriteLine(a[0]);
+            int[] a = new int[5] { 10, 20, 30, 40, 50 };
+            string[] b = new string[2] { "Shoaib", "Ali" };
+            foreach (int i in a)
+            {
+                Console.WriteLine(i);
+            }
+            foreach (string i in b)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
